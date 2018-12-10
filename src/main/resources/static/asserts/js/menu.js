f30975b68b5936c6b38b4cd2abfc9ac8f2d54a52
alexm <Alexmandr@ramble.ru>
2018-12-10 14:57:31 +0300

    add logos

---
+++ src/main/resources/static/asserts/js/menu.js
@@ -0,0 +1,54 @@
+(function() {
+// Find the Languages category.
+    var category = _.find(window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES,
+        {id: 'languages'});
+// Add Go as a new subcategory under Languages.
+    category.subCategories.splice(2, 0, { // Insert at the third spot.
+        // Required. Must be unique.
+        id: "go",
+        // Required.
+        label: "Go",
+        // Optional. If specified, defines a unique icon for this item.
+        icon: "icon-go-gopher",
+        // Required. Items matching any tag will appear in this subcategory.
+        tags: [
+            "go",
+            "golang"
+        ]
+    });
+
+// Add a Featured category as the first category tab.
+    window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.unshift({
+        // Required. Must be unique.
+        id: "featured",
+        // Required
+        label: "Featured",
+        subCategories: [
+            {
+                // Required. Must be unique.
+                id: "go",
+                // Required.
+                label: "Go",
+                // Optional. If specified, defines a unique icon for this item.
+                icon: "icon-go-gopher",
+                // Required. Items matching any tag will appear in this subcategory.
+                tags: [
+                    "go",
+                    "golang"
+                ]
+            },
+            {
+                // Required. Must be unique.
+                id: "jenkins",
+                // Required.
+                label: "Jenkins",
+                // Optional. If specified, defines a unique icon for this item.
+                icon: "icon-jenkins",
+                // Required. Items matching any tag will appear in this subcategory.
+                tags: [
+                    "jenkins"
+                ]
+            }
+        ]
+    });
+})
